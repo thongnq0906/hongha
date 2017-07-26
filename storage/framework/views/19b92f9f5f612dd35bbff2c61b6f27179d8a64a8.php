53cfb040ce36467e5a372b9aaf625bd708b4c949
DONG HUY VIET <donghuyviet@gmail.com>
2017-07-27 04:35:09 +0700

    update home

--- storage/framework/views/19b92f9f5f612dd35bbff2c61b6f27179d8a64a8.php
+++ storage/framework/views/19b92f9f5f612dd35bbff2c61b6f27179d8a64a8.php
@@ -2,22 +2,35 @@
 	<div class="footer ">
 		<div class=" uk-container uk-container-center">
 			<ul class= " menu-bottom uk-nav">
-			    <li class="my-id">
-				<a href="<?php echo e(url('/')); ?>">Trang chủ</a>	
-				</li>
-				<li class="<?php echo e(((Route::currentRouteName()=='tieu-khu')?'uk-active':'')); ?>" data-uk-dropdown><a href="<?php echo e(Route('tieu-khu')); ?>">Tiểu Khu</a></li>
-	            <li class="<?php echo e(((Route::currentRouteName()=='tien-ich')?'uk-active':'')); ?>"><a href="<?php echo e(Route('tien-ich')); ?>">Tiện ích</a></li>
-	            <li class="<?php echo e(((Route::currentRouteName()=='chinh-sach')?'uk-active':'')); ?>"><a href="<?php echo e(url('/')); ?>/tin-tuc/chinh-sach-vinhomes-riverside-the-harmony-cap-nhat.html">Chính sách</a></li>
+			    <?php $menu_lv1 = DB::table('categories')->where('parent_id',0)->get(); ?>
+	            <?php $__currentLoopData = $menu_lv1; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item1): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+	            <li class="" data-uk-dropdown>
+	                <?php if($item1->parent_id ==0): ?>
+	                    <a href="<?php echo e(route('page', $item1->slug)); ?>"><?php echo $item1->category; ?></a>
+	                <?php else: ?>
+	                <?php echo $item1->category; ?>
+
+	                <?php endif; ?>
+	                <div class="menu-childen uk-dropdown uk-dropdown-navbar">
+	                    <ul class="uk-nav uk-nav-navbar">
+	                        <?php $menu_lv2 = DB::table('categories')->where('parent_id',$item1->id)->get(); ?>
+	                        <?php $__currentLoopData = $menu_lv2; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item2): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+	                            <li><a href="<?php echo e(route('category', ['slug' => $item2->slug])); ?>"><?php echo $item2->category; ?></a></li>
+	                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+	                    </ul>
+	                </div>
+	            </li>
+	            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 	            <li class="<?php echo e(((Route::currentRouteName()=='quy-can-hot')?'uk-active':'')); ?>"><a href="<?php echo e(Route('quy-can-hot-frontend')); ?>">Quỹ căn hot</a></li>
-	            <li class="<?php echo e(((Route::currentRouteName()=='tin-tuc')?'uk-active':'')); ?>"><a href="<?php echo e(Route('tin-tuc')); ?>">Tin tức</a></li>
-	            <li class="<?php echo e(((Route::currentRouteName()=='lien-he2')?'uk-active':'')); ?>"><a href="<?php echo e(Route('lien-he2')); ?>">Liên hệ</a></li>
+
+	            <li class="<?php echo e(((Route::currentRouteName()=='lien-he2')?'uk-active':'')); ?>"><a href="<?php echo e(Route('lien-he2')); ?>">liên hệ</a></li>
 			</ul>
 			<div class=" info uk-grid">
 				
 				<div class=" item uk-width-large-1-3 uk-width-medium-1-3 uk-width-small-1-1">
 					<div class="title">CAM KẾT CỦA CHÚNG TÔI</div>
 					<ul class="">
-						<li>Queen Land là đại lý phân phối chính thức Vinhomes Riverside - The Harmony</li>
+						<li>Queen Land là đại lý phân phối chính thức Hồng Hà Eco City</li>
 						<li>Có quỹ căn đẹp đem tới cho quý khách hàng lựa chọn đa dạng phù hợp với nhu cầu và tài chính</li>
 						<li>Cung cấp thông tin chính xác và nhanh chóng nhất</li>
 						<li>Hỗ trợ tư vấn pháp lý và thủ tục nhanh chóng</li>
@@ -27,7 +40,6 @@
 				<div class="item uk-width-large-1-3 uk-width-medium-1-3 uk-width-small-1-1">
 					<div class="title">LIÊN HỆ VỚI CHÚNG TÔI</div>
 					<ul class="hotline">
-						<li><i class="uk-icon-map-marker"></i>Chi nhánh văn phòng: L2 - 202A - 205 TTTM Vincom Long Biên, Khu đô thị Vinhomes Riverside, Hà Nội</li>
 						<li><i class="uk-icon-map-marker"></i> Trụ sở chính: Tầng 1&2, Tòa Vimeco, Lô E9 đường Phạm Hùng, P. Trung Hòa, Q. Cầu Giấy, HN</li>
 						<li><a href="tel:0967876969" title="0967 87 6969"><i class="uk-icon-phone-square"></i>Hotline: 0967 87 6969</a></li>
 						<li><a href="tel:0473057666" title="04.7305.7666"><i class="uk-icon-phone"></i> Tel: 04.7305.7666</a></li>
@@ -35,7 +47,7 @@
 				</div>
 				<div class="item uk-width-large-1-3 uk-width-medium-1-3 uk-width-small-1-1">
 					<div class="maps">
-					<a href="tel:0967876969" title=""><img src="<?php echo e(asset('assets/images/bg_footer.jpg')); ?>"></a>
+					<a href="tel:0967876969" title=""><img src="<?php echo e(asset('samples/logo-queenland.png')); ?>"></a>
 					</div>
 				</div>
 			</div>
--- storage/framework/views/69ed3a3da53d9d87c6c128a1f50a629267482972.php
+++ storage/framework/views/69ed3a3da53d9d87c6c128a1f50a629267482972.php
@@ -1,18 +1,16 @@
 <nav class="tm-navbar uk-navbar uk-navbar-attached " data-uk-sticky="" style=" top: 0px; width: 100%; left: 0px;">
     <div class="uk-container uk-container-center data-uk-sticky">
 
-        <a class="uk-navbar-brand uk-hidden-small" href="<?php echo e(url('/')); ?>"><img class="uk-margin uk-margin-remove"
-                                                                             src="<?php echo e(asset('../assets/images/logo.png')); ?>"
-                                                                             width="90" height="30"
-                                                                             title="Vinhomes Riverside – The Harmony"
-                                                                             alt="Vinhomes Riverside – The Harmony"></a>
+        <a class="uk-navbar-brand uk-hidden-small" href="<?php echo e(url('/')); ?>">
+        <img class="uk-margin uk-margin-remove" src="<?php echo e(asset('../assets/images/logo.png')); ?>" width="90" 
+        height=" title= " Chung cư hồng hà eco city " alt="Chung cư hồng hà eco city" ></a>
 
         <ul class="uk-navbar-nav uk-hidden-small">
             <?php $menu_lv1 = DB::table('categories')->where('parent_id',0)->get(); ?>
             <?php $__currentLoopData = $menu_lv1; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item1): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <li data-uk-dropdown>
+            <li class="" data-uk-dropdown>
                 <?php if($item1->parent_id ==0): ?>
-                <a href="<?php echo e(route('page', $item1->slug)); ?>"> <?php echo $item1->category; ?></a>
+                    <a href="<?php echo e(route('page', $item1->slug)); ?>"><?php echo $item1->category; ?></a>
                 <?php else: ?>
                 <?php echo $item1->category; ?>
 
@@ -21,14 +19,16 @@
                     <ul class="uk-nav uk-nav-navbar">
                         <?php $menu_lv2 = DB::table('categories')->where('parent_id',$item1->id)->get(); ?>
                         <?php $__currentLoopData = $menu_lv2; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item2): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                        <li><a href="<?php echo e(route('category', ['slug' => $item2->slug])); ?>"><?php echo $item2->category; ?></a></li>
-                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-
+                            <li><a href="<?php echo e(route('category', ['slug' => $item2->slug])); ?>"><?php echo $item2->category; ?></a></li>
+                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                     </ul>
                 </div>
             </li>
             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-        </ul>
+            <li class="<?php echo e(((Route::currentRouteName()=='quy-can-hot')?'uk-active':'')); ?>"><a href="<?php echo e(Route('quy-can-hot-frontend')); ?>">Quỹ căn hot</a></li>
+
+            <li class="<?php echo e(((Route::currentRouteName()=='lien-he2')?'uk-active':'')); ?>"><a href="<?php echo e(Route('lien-he2')); ?>">liên hệ</a></li>
+            </ul>
         <ul class="hotline-top uk-nav uk-float-right uk-hidden-small">
             <li class="hotline"><a href="tel:0967876969"><span class="uk-icon-phone"></span>0967.87.69.69</a></li>
             <li class="hotline"><a href="tel:0916086688"><span class="uk-icon-phone"></span>0916.08.66.88</a></li>
@@ -45,42 +45,35 @@
                         alt="vinhomes riverside"></a></div>
 
     </div>
-    
-        
-            
-                
-                    
-                
-                
-                    
-                        
-                            
-                                        
-                                
-                            
-                                        
-                                
-                            
-                                        
-                                
-                            
-                                        
-                            
-                        
-                    
-                
-                
-                            
-                
-                            
-                        
-                
-                            
-                
-                            
-                
-                            
-            
-        
-    
+    <div id="tm-offcanvas" class="uk-offcanvas ng-scope">
+        <div class="uk-offcanvas-bar">
+            <ul class="uk-nav uk-nav-offcanvas uk-nav-parent-icon" data-uk-nav="">
+                <li class="my-id">
+                    <a href="<?php echo e(url('/')); ?>">Trang chủ</a>
+                </li>
+                <?php $menu_lv1 = DB::table('categories')->where('parent_id',0)->get(); ?>
+            <?php $__currentLoopData = $menu_lv1; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item1): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <li class="" data-uk-dropdown>
+                <?php if($item1->parent_id ==0): ?>
+                    <a href="<?php echo e(route('page', $item1->slug)); ?>"><?php echo $item1->category; ?></a>
+                <?php else: ?>
+                <?php echo $item1->category; ?>
+
+                <?php endif; ?>
+                <div class="menu-childen uk-dropdown uk-dropdown-navbar">
+                    <ul class="uk-nav uk-nav-navbar">
+                        <?php $menu_lv2 = DB::table('categories')->where('parent_id',$item1->id)->get(); ?>
+                        <?php $__currentLoopData = $menu_lv2; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item2): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <li><a href="<?php echo e(route('category', ['slug' => $item2->slug])); ?>"><?php echo $item2->category; ?></a></li>
+                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                </div>
+            </li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <li class="<?php echo e(((Route::currentRouteName()=='quy-can-hot')?'uk-active':'')); ?>"><a href="<?php echo e(Route('quy-can-hot-frontend')); ?>">Quỹ căn hot</a></li>
+
+            <li class="<?php echo e(((Route::currentRouteName()=='lien-he2')?'uk-active':'')); ?>"><a href="<?php echo e(Route('lien-he2')); ?>">liên hệ</a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
